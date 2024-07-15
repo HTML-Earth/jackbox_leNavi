9fd53b6ef796b573e8aac5e95b2f181be20641f1
HTML_Earth <htmlearth@gmail.com>
2024-07-15 16:03:05 +0200

    Added script to update json based on spreadsheets, and updated AwShirtSlogans using it

---
+++ update-json-from-tsv.ps1
@@ -0,0 +1,18 @@
+$jsonPath = $args[0]
+$tsvPath = $args[1]
+
+$jsonContent = Get-Content $jsonPath -Raw | ConvertFrom-Json
+$tsvContent = Import-Csv $tsvPath -Delimiter '	'
+
+foreach ($row in $tsvContent)
+{
+    if ($row."na'vi" -ne "")
+    {
+        $matchingId = $jsonContent.content | Where-Object { $_.id -eq $row.id }
+        $matchingId.suggestion = $row."na'vi"
+
+        Write-Output "Replaced $($row.id)"
+    }
+}
+
+$jsonContent | ConvertTo-Json -Depth 4 | Out-File $jsonPath -Encoding utf8NoBOM
